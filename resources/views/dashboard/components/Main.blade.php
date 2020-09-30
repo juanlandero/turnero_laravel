fc4f712d709835a2e12014647ea0e426fbd62cbe
Jorge Cortés <georgeluis.idem@gmail.com>
2020-09-29 20:37:31 -0500

    Maquetación de Dashboard y Login

---
+++ resources/views/dashboard/components/Main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>{{$_PAGE_TITLE}} | @yield('title', '*** TITLE ***')</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        @yield('dashboard.components.Stylesheets')
+    </head>
+    <body class="app sidebar-mini">
+        @yield('dashboard.components.Navbar')
+
+        @yield('dashboard.components.Sidebar')
+
+        <main class="app-content">
+            @yield('content', '*** CONTENT ***')
+        </main>
+
+        @yield('dashboard.components.Scripts')
+    </body>
+</html>
