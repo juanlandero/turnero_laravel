cb6356efe43ef36d9904ffe68d36223bc80c4dcf
Juan Carlos Landero <jcarlos210193@gmail.com>
2020-11-05 22:39:23 -0600

    Asesores con errores

--- app/Http/Controllers/Dashboard/UserAdviserController.php
+++ app/Http/Controllers/Dashboard/UserAdviserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Library\Returns\ActionReturn;
 use App\Library\Errors;
 use App\Library\Messages;
+use App\UserPrivilege;
 use App\User;
 
 class UserAdviserController extends Controller
@@ -18,5 +19,53 @@ class UserAdviserController extends Controller
 
     public function create() {
         return View('dashboard.contents.users.advisers.Create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'txtName'           => 'required|string|max:255',
+            'txtFirstName'      => 'required|string|max:50',
+            'txtSecondName'     => 'required|string|max:50',
+            'txtEmail'          => 'required|string|unique:users,email|max:80',
+            'txtPassword'       => 'required|string|max:20'
+        ],[
+            'txtEmail.unique'   => 'El correo ingresado ya pertenece a otro usuario.'
+        ]);
+
+        $objReturn = new ActionReturn('dashboard/users-advisers/create', 'dashboard/users-advisers');
+
+        $objUser                  = new User();
+        $objUser->name            = $request->txtName;
+        $objUser->first_name      = $request->txtFirstName;
+        $objUser->second_name     = $request->txtSecondName;
+        $objUser->user_type_id    = 3;
+        $objUser->email           = $request->txtEmail;
+        $objUser->password        = bcrypt($request->txtPassword);
+        $objUser->is_active       = true;
+
+        try {
+            if($objUser->save()) {
+                $privileges = array();
+                  
+                  for($i = 1; $i < 25; $i++) {
+                      if($i != 22) {
+                          array_push($privileges, array(
+                            'user_id'         => $objUser->id,
+                            'privilege_id'    => $i,
+                          ));
+                      }
+                  }
+
+                  UserPrivilege::create($privileges);
+
+                $objReturn->setResult(true, Messages::USER_ADMIN_CREATE_TITLE, Messages::USER_ADMIN_CREATE_MESSAGE);
+            } else {
+                $objReturn->setResult(false, Errors::USER_CREATE_01_TITLE, Errors::USER_CREATE_01_MESSAGE);
+            }
+        } catch(Exception $exception) {
+            $objReturn->setResult(false, Errors::getErrors($exception->getCode())['title'], Errors::getErrors($exception->getCode())['message']);
+        }
+
+        return $objReturn->getRedirectPath();
     }
 }
